2716b6b0271745a1bf7bda18f63d0c6cd6578b83
xianwew <50687225+xianwew@users.noreply.github.com>
2023-10-17 17:30:22 -0400

    upload yup

--- cs5704frontend/src/App.js
+++ cs5704frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Login from './Pages/Login';
 import Library from './Pages/Library';
 
+
 function App() {
   return (
     <div className='full-screen'>
--- cs5704frontend/src/Pages/Login.js
+++ cs5704frontend/src/Pages/Login.js
@@ -7,35 +7,90 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { useNavigate } from "react-router-dom";
 import '../CSS/general.css';
+import * as Yup from "yup";
+import { Formik, Form, Field } from "formik";
 
 export default function Login() {
     const navigate = useNavigate();
 
-    const handleLoginClick = () => {
+    const initialValues = {
+        email: "",
+        password: "",
+    };
+
+    const validationSchema = Yup.object().shape({
+        email: Yup.string().email('Invalid email format').required("You must input an email"),
+        password: Yup.string().min(8).max(25).required("You must input a password"),
+    });
+
+    const handleSubmit = (values) => {
+        // Simulating a successful login
         navigate('/lib');
-      };
+    };
 
     return (
-        <Card elevation={3} className='Card' sx={{ width: "450px", height: "350px"}}>
-            <div style={{position: "absolute", bottom: "0", right: "0", left: "0"}}>
-                <p style={{textAlign: "center", marginBottom: "20px", fontSize: "50px", fontWeight: "bold", fontFamily: "Roboto", textShadow: "4px 4px #5a8eda", color: "#738dd7"}}>Welcome</p>
-                <CardContent>
-                    <Box
-                        sx={{
-                            display: 'flex',
-                            flexDirection: 'column',
-                        }}
-                    >
-                        <TextField label={'Email Address'} style={{marginBottom: "25px"}}/>
-                        <TextField label={'Password'} style={{marginBottom: "10px"}}/>
-                    </Box>
-                </CardContent>
-                <CardActions>
-                    <Button size="small" style={{ position: "absolute", right: "10px" }} onClick={handleLoginClick}>Login</Button>
-                    <Button size="small" style={{ marginLeft: "auto", marginRight: "70px" }}>Create Account</Button>
-                    <Button size="small" style={{ position: "absolute", left: "10px" }}>Forgot Password?</Button>
-                </CardActions>
+        <Card elevation={3} className='Card' sx={{ width: "450px", height: "380px" }}>
+            <div style={{ position: "absolute", bottom: "0", right: "0", left: "0" }}>
+                <Formik
+                    initialValues={initialValues}
+                    validationSchema={validationSchema}
+                    onSubmit={handleSubmit}
+                >
+                    {({ errors, touched }) => (
+                        <Form>
+                            <p style={{
+                                textAlign: "center",
+                                marginBottom: "0px",
+                                fontSize: "50px",
+                                fontWeight: "bold",
+                                fontFamily: "Roboto",
+                                textShadow: "4px 4px #5a8eda",
+                                color: "#738dd7"
+                            }}>Welcome</p>
+
+                            <CardContent>
+                                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                                    <div className="login-error-message">
+                                        {touched.email && errors.email && errors.email}
+                                    </div>
+                                    <Field
+                                        as={TextField}
+                                        label='Email Address'
+                                        name="email"
+                                        fullWidth
+                                        style={{ marginBottom: "25px" }}
+                                    />
+
+                                    <div className="login-error-message">
+                                        {touched.password && errors.password && errors.password}
+                                    </div>
+                                    <Field
+                                        as={TextField}
+                                        label='Password'
+                                        name="password"
+                                        type="password"
+                                        fullWidth
+                                        style={{ marginBottom: "10px" }}
+                                    />
+                                </Box>
+                            </CardContent>
+
+                            <CardActions>
+                                <Button type="submit" size="small" style={{ position: "absolute", right: "10px" }}>
+                                    Login
+                                </Button>
+                                <Button size="small" style={{ marginLeft: "auto", marginRight: "70px" }}>
+                                    Create Account
+                                </Button>
+                                <Button size="small" style={{ position: "absolute", left: "10px" }}>
+                                    Forgot Password?
+                                </Button>
+                            </CardActions>
+                        </Form>
+                    )}
+                </Formik>
             </div>
         </Card>
     );
 }
+
