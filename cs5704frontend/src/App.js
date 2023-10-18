6db1a4b30214d76d6ab9b4accc48a7e6d6c2e5d5
xianwew <50687225+xianwew@users.noreply.github.com>
2023-10-17 20:48:29 -0400

    update reset/create password

--- cs5704frontend/src/App.js
+++ cs5704frontend/src/App.js
@@ -2,16 +2,20 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Login from './Pages/Login';
 import Library from './Pages/Library';
-
+import CreateResetAccount from './Pages/CreateResetAccount';
+import { useState } from 'react';
 
 function App() {
+  const [accountTitle, setAccountTitle] = useState("Create Account");
+
   return (
     <div className='full-screen'>
       <div className='App'>
         <Router>
           <Routes>
-            <Route path="/" element={<Login/>} />
+            <Route path="/" element={<Login setAccountTitle={setAccountTitle}/>} />
             <Route path="/lib" element={<Library/>} />
+            <Route path="/setUpAccount" element={<CreateResetAccount title={accountTitle}/>} />
           </Routes>
         </Router>
       </div>
--- cs5704frontend/src/Pages/CreateResetAccount.js
+++ cs5704frontend/src/Pages/CreateResetAccount.js
@@ -6,75 +6,36 @@ import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { useNavigate } from "react-router-dom";
-import CreateResetAccount from './CreateResetAccount';
 import '../CSS/general.css';
 import * as Yup from "yup";
 import { Formik, Form, Field } from "formik";
 
-export default function Login({setAccountTitle}) {
+export default function CreateResetAccount({ title }) {
     const navigate = useNavigate();
 
     const initialValues = {
         email: "",
         password: "",
+        passwordConfirmed: "",
+        verificationCode: "",
     };
 
     const validationSchema = Yup.object().shape({
         email: Yup.string().email('Invalid email format').required("You must input an email"),
         password: Yup.string().min(8).max(25).required("You must input a password"),
+        passwordConfirmed: Yup.string()
+            .oneOf([Yup.ref('password'), null], 'Passwords must match')
+            .required('Confirm password is required'),
+        verificationCode: Yup.string().required("You must input the verification code"),
     });
 
-    const handleCreateAccount = (values) => {
-        setAccountTitle("Create Account");
-        navigate('/setUpAccount');
-    }
-
-    const handleResetAccount = (values) => {
-        setAccountTitle("Reset Password");
-        navigate('/setUpAccount');
-    }
-
-    const handleSubmit = async (values) => {
-        const credentials = {
-            userAccount: values.email,
-            userPassword: values.password,
-        };
-
-        try {
-            const response = await fetch('http://localhost:8080/api/user/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(credentials),
-            });
-
-            const responseData = await response.json();
-
-            if (response.ok) {
-                // 检查 HTTP 状态代码是否指示成功（2xx）,因为后台的状态码不是用的标准版，而是多了两位，如20011，40001，所以需要取前三位
-                const codePrefix = Math.floor(responseData.code / 100); // 计算代码的前三位
-                console.log(codePrefix)
-                if (codePrefix === 200) {
-                    // 如果代码以 200 开头，处理成功的情况
-                    console.log('Request was successful:', responseData);
-                    navigate('/lib');
-                } 
-                else {
-                    console.error('Something went wrong:', responseData);
-                }
-            } 
-            else {
-                throw new Error(`Bad response from server: ${response.status}`);
-            }
-        } catch (error) {
-            console.error('There was a problem with the fetch operation:', error);
-        }
+    const handleSubmit = (values) => {
+        // Simulating a successful login
+        navigate('/lib');
     };
 
-
     return (
-        <Card elevation={3} className='Card' sx={{ width: "450px", height: "380px" }}>
+        <Card elevation={3} className='Card' sx={{ width: "450px", height: "550px" }}>
             <div style={{ position: "absolute", bottom: "0", right: "0", left: "0" }}>
                 <Formik
                     initialValues={initialValues}
@@ -91,7 +52,7 @@ export default function Login({setAccountTitle}) {
                                 fontFamily: "Roboto",
                                 textShadow: "4px 4px #5a8eda",
                                 color: "#738dd7"
-                            }}>Welcome</p>
+                            }}>{title}</p>
 
                             <CardContent>
                                 <Box sx={{ display: 'flex', flexDirection: 'column' }}>
@@ -117,18 +78,41 @@ export default function Login({setAccountTitle}) {
                                         fullWidth
                                         style={{ marginBottom: "10px" }}
                                     />
+
+                                    <div className="login-error-message">
+                                        {touched.PasswordConfirmed && errors.PasswordConfirmed && errors.PasswordConfirmed}
+                                    </div>
+                                    <Field
+                                        as={TextField}
+                                        label='confirm the password'
+                                        name="PasswordConfirmed"
+                                        type="PasswordConfirmed"
+                                        fullWidth
+                                        style={{ marginBottom: "10px" }}
+                                    />
+
+                                    <div className="login-error-message">
+                                        {touched.confirmation && errors.confirmation && errors.confirmation}
+                                    </div>
+                                    <Field
+                                        as={TextField}
+                                        label='email confirmation'
+                                        name="confirmation"
+                                        type="confirmation"
+                                        fullWidth
+                                    />
                                 </Box>
                             </CardContent>
 
                             <CardActions>
-                                <Button onClick = {handleResetAccount} size="small" style={{ marginLeft: "8px", marginBottom: "5px" }}>
-                                    Forgot Password?
+                                <Button type="submit" size="small" style={{marginLeft: "8px", marginBottom: "5px"}}>
+                                    Send confirmation code
                                 </Button>
-                                <Button onClick = {handleCreateAccount} size="small" style={{ marginLeft: "80px", marginBottom: "5px" }}>
+                                <Button size="small" onClick = {() => {navigate('/');}} style={{marginLeft: "40px", marginBottom: "5px"}}>
+                                    back
+                                </Button>
+                                <Button size="small" style={{marginLeft: "5px", marginBottom: "5px"}}>
                                     Create Account
-                                </Button>
-                                <Button type="submit" size="small" style={{  marginLeft: "5px", marginBottom: "5px" }}>  
-                                    Login
                                 </Button>
                             </CardActions>
                         </Form>
