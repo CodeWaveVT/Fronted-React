484ada4d06e5da8445d3eaeec8f72caf5fba921e
xianwew <50687225+xianwew@users.noreply.github.com>
2023-11-20 19:59:11 -0500

    Update App.js

--- cs5704frontend/src/App.js
+++ cs5704frontend/src/App.js
@@ -36,13 +36,13 @@ function App() {
         barOpen={snackBarOpen}
         setBarOpen={setSnackBarOpen}
         alertType={3}
-        hideDuration={3000}
+        hideDuration={4000}
       />
         <SnackBar
         barOpen={loggedInSnack}
         setBarOpen={setLoggedIn}
         alertType={2}
-        hideDuration={3000}
+        hideDuration={4000}
       />
       <div className='App'>
         <Router>
